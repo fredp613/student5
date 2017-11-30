1ba249da08a8c6911576078171ba899e6b2e7747
Student Administrator <fredp613@gmail.com>
2017-11-30 11:55:26 -0500

    crm integration work

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace student5.Models
 {
@@ -11,5 +12,12 @@ namespace student5.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [NotMapped]
+        public string FullName {
+            get {
+                    return FirstName + " " + LastName;
+            } set {
+            }
+        }
     }
 }
